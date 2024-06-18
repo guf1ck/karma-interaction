0909f371d8d0877563c0dccbe40db9058ef928bd
Rafael Antunes <151884955+guf1ck@users.noreply.github.com>
2024-06-18 17:21:13 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,17 +2,15 @@ fx_version 'cerulean'
 games { 'gta5' }
 author 'ANTUNES'
 description 'Karma Developments Interaction System 4.0'
-client_scripts {
-    "client/cl_*.lua"
-}
-
-shared_scripts {
-    "shared/sh_*.lua"
-}
 
 ui_page 'web/web.html'
 
 files {
     'web/*.*',
     'web/fonts/*.*',
+}
+
+
+client_scripts {
+    "client/cl_*.lua"
 }
